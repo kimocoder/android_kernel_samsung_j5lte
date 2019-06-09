16af0e742e3754abaeb610bccb93123a3c37f073
amrwb <yibyuk@icloud.com>
2019-06-09 15:53:19 +0900

    2017-09-01 CVE Patch

--- output/include/generated/compile.h
+++ output/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 4 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Sun May 5 23:05:23 KST 2019"
+#define UTS_VERSION "#4 SMP PREEMPT Sun Jun 9 15:40:25 KST 2019"
 #define LINUX_COMPILE_BY "volte"
 #define LINUX_COMPILE_HOST "volte-HP-Notebook"
 #define LINUX_COMPILER "gcc version 4.8 (GCC) "
--- output/include/generated/utsrelease.h
+++ output/include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.10.49-g9bb36c6c-dirty"
+#define UTS_RELEASE "3.10.49-gd55e5d74-dirty"
